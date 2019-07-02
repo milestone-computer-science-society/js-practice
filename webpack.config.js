56c15350d934f8ea0f7c4f20d515f3cd9c817b4e
Ádám András <adm.andras@gmail.com>
2019-07-03 00:48:41 +0800

    Added Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,26 @@
+// Webpack Configuration
+
+const path = require('path');
+
 module.exports = {
-  
-}
+  mode: 'development',
+
+  entry: ['babel-polyfill', './src/main.js'],
+
+  output: {
+    path: path.join(__dirname, '/'),
+    filename: 'dist/bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+
+};
