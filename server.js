92b226782cfca14c1f84353659de3ecc7e6ba108
Dani Foldi <dani@stefan.hu>
2019-07-05 01:04:28 +0800

    Require missing module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const tester = require('./testing.js')
 const challenges = require('./challenges/javascript.js')
