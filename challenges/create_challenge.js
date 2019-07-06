59280725ea5485a16d89f39f687c064542f29ed9
Dani Foldi <dani@stefan.hu>
2019-07-06 11:31:09 +0800

    Added challenge creator helper

---
+++ challenges/create_challenge.js
@@ -0,0 +1,46 @@
+const prompt = require('prompt')
+
+const challenge_schema = {
+  properties: {
+    challenge: {
+      type: 'string',
+      required: true,
+      default: ''
+    },
+    title: {
+      type: 'string',
+      required: true
+    },
+    description: {
+      type: 'string',
+      required: true
+    },
+    task: {
+      type: 'string',
+      required: true
+    },
+    mode: {
+      type: 'string',
+      required: true,
+      default: 'vm'
+    }
+  }
+}
+
+prompt.start()
+prompt.get(challenge_schema, (err, result) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(`
+    ${result.challenge}: {
+      title: '${result.title}',
+      description: '${result.description}',
+      task: '${result.task}',
+      verify: output => {
+
+      },
+      mode: '${result.mode}'
+    },`)
+})
